Fixed sensor covariance

assmts/assmt1/__main__.py
@@ -109,6 +109,7 @@ def main():
             [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
         ]
     )
+    print(process_noise_covariance)
     kf = KalmanFilter(
         matrix_transform=matrix_transform,
         process_noise_covariance=process_noise_covariance,
@@ -118,7 +119,7 @@ def main():
         [start_pos.x, start_pos.y, start_pos.z, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ).reshape(-1, 1)
 
-    estimate_uncertainty = np.random.normal(size=init_state.shape)
+    estimate_uncertainty = np.random.normal(size=init_state.shape).T
     kf.init_state(
         t=time[0], state=init_state, estimate_uncertainty=estimate_uncertainty
     )

estimators/kalman_filter.py
@@ -100,19 +100,21 @@ class KalmanFilter:
         """
         Extrapolate the uncertainty of the system at time t
         """
-        state_matrix = (
+        state_transform = (
             state_transition_transform
             if state_transition_transform is not None
             else self._state_transition_transform
         )
         print(
-            state_matrix.shape,
+            state_transform.shape,
             self._estimate_uncertainty[self._prev_t].shape,
             self._process_noise_covariance.shape,
         )
         self._estimate_uncertainty_pred[t] = np.diag(
             np.diag(
-                state_matrix @ self._estimate_uncertainty[self._prev_t] @ state_matrix.T
+                state_transform
+                @ self._estimate_uncertainty[self._prev_t]
+                @ state_transform.T
                 + self._process_noise_covariance
             )
         )